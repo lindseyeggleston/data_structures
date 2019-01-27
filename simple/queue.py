6a461ba2933dd43170d53f5fb6ae83ab573d6ab1
Optfit <optfit@pop-os.localdomain>
2019-01-27 15:04:34 -0700

    rename queue methods

--- simple/queue.py
+++ simple/queue.py
@@ -1,42 +1,62 @@
 class Queue():
     """ A Queue is a dynamic array-like data structure that follows
     the principle of FIFO (or first in, first out). All elements of
-    the Queue must be of the same data type. Note that the Queue 
-    will attempt to coerce inputs to the same type as the first
-    element.
+    the Queue must be of the same data type. Note, the Queue will
+    attempt to coerce inputs to the same type as the first element.
 
     Args:
         data: int, float, str, or array-like of same types
+        max_size: int - max number of elements allowed in the Queue
         dtype: specific data type for elements in the Queue
 
     Methods:
-        push(x): adds x to end of Queue
-        pop(): removes first element from Queue
+        enqueue(x): adds x to the tail of Queue
+        dequeue(): removes element at the head of the Queue
+        peek(): views element at the head of the Queue without
+            removing it
         is_empty(): reports whether the Queue is empty or not
+        is_full(): checks if queue is full or not
     """
-    def __init__(self, data=None, dtype=None):
+    def __init__(self, data=None, max_size: int = None, dtype=None):
         self._data = []
-        self._dtype = None
+        self._max_size = max_size
+        if dtype in {None, int, float, tuple}:
+            self._dtype = dtype
+        else:
+            raise TypeError('Queue only supports int, float, and str types')
         if data:
             if isinstance(data, (list, tuple)):
                 for element in data:
-                    self.push(element)
+                    self.enqueue(element)
             else:
-                self.push(data)
+                self.enqueue(data)
 
-    def push(self, x):
-        x = self._check_type(x)
-        self._data.append(x)
+    def enqueue(self, x):
+        if not self.is_full():
+            x = self._check_type(x)
+            self._data.append(x)
+        else:
+            raise OverflowError('Queue is full and cannot be added to.')
 
-    def pop(self):
+    def dequeue(self):
         return self._data.pop(0)
+
+    def peek(self):
+        if not self.is_empty():
+            return self._data[0]
 
     def is_empty(self):
         return len(self) == 0
+
+    def is_full(self):
+        if self._max_size:
+            if len(self) >= self._max_size:
+                return True
+        return False
     
     def _check_type(self, x):
         if not isinstance(x, (int, float, str)):
-            raise TypeError('Stack only supports int, float, and str types')
+            raise TypeError('Queue only supports int, float, and str types')
         if self.is_empty() and not self._dtype:
             self._dtype = type(x)
         try:
